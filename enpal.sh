60ca2c276d372ad2ff58bd547b30e4d571ddef93
ToniValac <hours_07_stacks@icloud.com>
2024-05-21 18:39:54 +0200

    Found the issue

--- enpal.sh
+++ enpal.sh
@@ -13,14 +13,14 @@ output_whole_bucket() {
   echo "QUERY_RANGE_START: ${QUERY_RANGE_START}"  # Debug statement
 
   # Perform the curl request and capture the HTTP response headers
-  response=$(curl -i -s -w "%{http_code}" "${INFLUX_API}" \
+  response=$(curl -s -X POST "${INFLUX_API}" \
     --header "Authorization: Token ${INFLUX_TOKEN}" \
     --header "Accept: application/json" \
     --header "Content-type: application/json" \
     --data-binary @- <<EOF
 {
   "type": "flux",
-  "query": "from(bucket: \\"${INFLUX_BUCKET}\\") |> range(start: ${QUERY_RANGE_START}) |> filter(fn: (r) => r._measurement == \\"numberDataPoints\\") |> filter(fn: (r) => r._field == \\"Power.Production.Total\\") |> keep(columns: [\\"_time\\", \\"_value\\", \\"_field\\"]) |> last()",
+  "query": "from(bucket: \\"${INFLUX_BUCKET}\\") |> range(start: ${QUERY_RANGE_START}) |> last()",
   "orgID": "${INFLUX_ORG_ID}"
 }
 EOF
@@ -35,10 +35,12 @@ EOF
 
   if [ "$status" -eq 0 ] && [ "$http_code" -eq 200 ]; then
     echo "$(date '+%Y-%m-%d %H:%M:%S') - Data query successful." >> /var/log/enpal.log
+
+    # Parse the JSON response and format the output
+    echo "$body" | jq -r '.results[0].series[0].values[0] | to_entries | map("\(.key): \(.value) \(.unit // "")") | .[]'
   else
     echo "$(date '+%Y-%m-%d %H:%M:%S') - Data query failed with status $status and HTTP code $http_code." >> /var/log/enpal.log
   fi
-  echo "$body"
   return "$status"
 }
 
