83526bfe206a31b6cbc0641045fd1be7e333c8ff
ToniValac <hours_07_stacks@icloud.com>
2024-05-21 16:52:06 +0200

    Logging

--- enpal.sh
+++ enpal.sh
@@ -5,6 +5,7 @@ QUERY_RANGE_START="-5m"
 
 # Function to output the whole bucket
 output_whole_bucket() {
+  echo "$(date '+%Y-%m-%d %H:%M:%S') - Starting data query..." >> /var/log/enpal.log
   var=$(curl -f -s "${INFLUX_API}" \
     --header "Authorization: Token ${INFLUX_TOKEN}" \
     --header "Accept: application/json" \
@@ -12,6 +13,11 @@ output_whole_bucket() {
     --data "from(bucket: \"$INFLUX_BUCKET\")
             |> range(start: $QUERY_RANGE_START)")
   status="$?"
+  if [ "$status" -eq 0 ]; then
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - Data query successful." >> /var/log/enpal.log
+  else
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - Data query failed with status $status." >> /var/log/enpal.log
+  fi
   echo "$var"
   return "$status"
 }
@@ -21,4 +27,3 @@ while true; do
   output_whole_bucket
   sleep 60  # Wait for 1 minute before the next iteration
 done
-
